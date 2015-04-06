6cd8354687c8f0b42b523e2b3a16a8e366048315
georgia bullen <georgia.bullen@gmail.com>
2015-04-05 21:54:54 -0400

    comment out the record line on the customize-maps view

--- _includes/tutorial-maps-customize.js
+++ _includes/tutorial-maps-customize.js
@@ -8,7 +8,7 @@ view.geoJsonLayerOptions.pointToLayer = function(feature, latlng) {
   // Look up Record so we can use it to customize size of marker
   // note that 'this' is specially bound for us to parent view + that feature
   // stores record cid
-  var record = this.model.records.getByCid(feature.properties.cid);
+  // var record = this.model.records.getByCid(feature.properties.cid);
   var marker = new L.CircleMarker(latlng, { radius: record.get('x') * 3 });
   marker.bindPopup(feature.properties.popupContent);
   return marker;
